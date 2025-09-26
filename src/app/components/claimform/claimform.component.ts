bece630f178869147889a19461ede2762368d392
Kirtanadevi23 <Vetrivelk@nordex-ag.com>
2025-09-26 14:57:18 +0530

    claim commit

---
+++ src/app/components/claimform/claimform.component.ts
@@ -0,0 +1,52 @@
+import { CommonModule, NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ClarityModule } from '@clr/angular';
+
+@Component({
+  selector: 'app-claimform',
+  standalone: true,
+  imports: [ClarityModule,CommonModule,FormsModule],
+  templateUrl: './claimform.component.html',
+  styleUrl: './claimform.component.css'
+})
+export class ClaimformComponent {
+
+modalOpen = false;
+  today: string = '';
+  username: string = 'Arun Kumar'; // Placeholder for Windows username
+
+  formData = {
+    employeeCode: '',
+    purposePlace: '',
+    companyPlant: '',
+    costCenter: '',
+    vendorCode: ''
+  };
+
+  ngOnInit() {
+    const now = new Date();
+    this.today = now.toLocaleDateString('en-GB', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric'
+    });
+  }
+
+  openModal() {
+    this.modalOpen = true;
+  }
+
+  closeModal(form: any) {
+    this.modalOpen = false;
+    form.resetForm();
+  }
+
+  onNext(form: any) {
+    if (form.valid) {
+      alert('Form submitted:\n' + JSON.stringify(this.formData, null, 2));
+      this.closeModal(form);
+    }
+  }
+}
+
