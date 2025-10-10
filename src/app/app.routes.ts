55a020f733269b4403ef759f1bcd016d85065270
Kirtanadevi23 <Vetrivelk@nordex-ag.com>
2025-10-10 11:40:02 +0530

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ClaimformComponent } from './components/claimform/claimform.component';
 import { ExpenseComponent } from './components/expense/expense.component';
 import { ExpensereviewComponent } from './components/expensereview/expensereview.component';
+import { InternationalformComponent } from './components/internationalform/internationalform.component';
 
 export const routes: Routes = [
     {path:'',component:ClaimformComponent},
     {path:'expense',component:ExpenseComponent},
-    {path:'expensereview',component:ExpensereviewComponent}
-];
+    {path:'expensereview',component:ExpensereviewComponent},
+    {path:'international',component:InternationalformComponent}
+]
